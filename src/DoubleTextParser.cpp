5f4ad60777359c565b89595bda242fbcf47f8ebd
David Marcano <david.marcano@yale.edu>
2013-12-24 21:16:27 +0100

    fixing the fstream pointer

--- src/DoubleTextParser.cpp
+++ src/DoubleTextParser.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <iostream>
 
 DoubleTextParser::DoubleTextParser(fstream *apFile)
 :mpFile = apFile
@@ -18,31 +19,33 @@ int_fast32_t* DoubleTextParser::parse()
 {
 	int_fast32_t * image = new int [480][640];
 
-	ifstream img_file ("stuff.txt");
+	if (!*mpFile.is_open())
+		*mpFile.open( , ios::in)
+
 
 	int_fast32_t i = 0;
 	int_fast32_t j = 0;
-	while (img_file)
+	if (*mpFile)
 	{
 		string s;
 		/*read into image considering parsing by commas for rows and newlines for columns*/
-		if (!getline(img_file, s))
-			break;
-		istringstream holder(s);
-
-		while (holder)
+		while (getline(*mpFile, s))
 		{
-			string s;
-			if (!getline(holder, s, ',')
-				break;
-			size_t period = s.find('.');
-			if (period != string::npos)
-				s.erase(period);
-			image[i][j] = s;
-			j++;
-		}
-		i++;
+			istringstream holder(s);
 
+			while (holder)
+			{
+				string s;
+				if (!getline(holder, s, ',')
+					break;
+				size_t period = s.find('.');
+				if (period != string::npos)
+					s.erase(period);
+				image[i][j] = s;
+				j++;
+			}
+			i++;
+		}
 
 	}	
 	return (int_fast32_t*)(0); 

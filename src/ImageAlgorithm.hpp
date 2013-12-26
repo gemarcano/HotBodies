c15c658e379222c7b64e9c6b18afbf6611b2aa63
Gabriel Marcano <gabemarcano@yahoo.com>
2013-12-25 22:38:30 -0500

    Skeleton of Logic/Processing layer.

---
+++ src/ImageAlgorithm.hpp
@@ -0,0 +1,10 @@
+#ifndef _IMAGEALGORITHM_H_
+#define _IMAGEALGORITHM_H_
+
+class ImageAlgorithm
+{
+
+
+};
+
+#endif //_IMAGEALGORITHM_H_
---
+++ src/SortImageAlgorithm.hpp
@@ -0,0 +1,11 @@
+#ifndef _SORTIMAGEALGORITHM_H_
+#define _SORTIMAGEALGORITHM_H_
+
+#include "ImageAlgorithm.hpp"
+
+class SortImageAlgorithm : public ImageAlgorithm
+{
+	
+};
+
+#endif //_SORTIMAGEALGORITHM_H_

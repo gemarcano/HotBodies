2826f6b1bfd1b3b9837c6efaac899e3e6d0eb641
Gabriel Marcano <gabemarcano@yahoo.com>
2013-12-29 00:29:36 -0500

    Further integration of gtest with "make check." Still more work to be done. gtest proper needs to be linked to the build system, and the issue of pthread with relation to linking gtest and hotbodies' tests needs to be looked into.

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
+TEST(CHECKTEST, TEST0)
+{
+	EXPECT_EQ(1,0);
+}
+
 int main (int argc, char *argv[])
 {
-	
-	return 0;
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
